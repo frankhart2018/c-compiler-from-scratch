74dce917077129bbefa208bf2da6d796eeba1888
frankhart <siddharthadhar.soumen@srmuniv.edu.in>
2021-04-08 18:06:53 +0530

    Parallelize tests

--- run_all_tests.py
+++ run_all_tests.py
@@ -1,19 +1,40 @@
 import subprocess
 import os
 import glob
+import subprocess
 
+
+def run_parallel_test(paths):
+    outs = [subprocess.Popen(f'cd {path}; make .PHONY', 
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             shell=True) for path in paths]
+
+    for out in outs:
+        out.wait()
+
+    for j, out in enumerate(outs):
+        output = str(out.stdout.read())
+        if "tests passed!" not in output:
+            print(f"\033[91mTest for {os.path.basename(paths[j])} failed!\033[m")
+        else:
+            print(f"\033[92mTest for {os.path.basename(paths[j])} passed!\033[m")
 
 dirs = glob.glob("commit-*")
 dirs = sorted(dirs, key=lambda x: int(x.split("-")[1]))
 base_path = os.getcwd()
+executed_tests = 0
+NUM_PARALLEL_TESTS = 4
 
-for dir in dirs:
-    full_dir_path = os.path.join(base_path, dir)
-    os.chdir(full_dir_path)
+for i in range(0, len(dirs), NUM_PARALLEL_TESTS):
+    try:
+        paths = [os.path.join(base_path, dirs[i+j]) for j in range(NUM_PARALLEL_TESTS)]
+    except:
+        break
 
-    output = subprocess.getoutput("make .PHONY")
-    output = output.split("\n")[-2]
-    if output == "OK":
-        print(f"\033[92mTest for {dir.replace('-', ' ')} passed\033[m")
-    else:
-        print(f"\033[91mTest for {dir.replace('-', ' ')} failed\033[m")
+    executed_tests += len(paths)
+    
+    run_parallel_test(paths=paths)
+
+remaining_paths = dirs[executed_tests:] if executed_tests != len(dirs) else []
+run_parallel_test(remaining_paths)

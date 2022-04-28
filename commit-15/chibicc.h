d8fc5bf4b11dba40a52bdd0b8e5e91adcfae2a8b
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2022-04-28 01:23:02 -0400

    Add commit 15

---
+++ commit-15/chibicc.h
@@ -0,0 +1,131 @@
+#ifndef CHIBICC_H
+#define CHIBICC_H
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <assert.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node Node;
+
+//
+// Consts
+//
+typedef struct {
+    bool requires_le_function;
+    bool requires_leq_function;
+    bool requires_ne_function;
+} Const;
+
+Const init_const();
+
+//
+// tokenize.c
+//
+
+// Token
+typedef enum {
+    TK_IDENT,       // Identifiers
+    TK_PUNCT,       // Punctuators
+    TK_KEYWORD,     // Keywords
+    TK_NUM,         // Numeric literals
+    TK_EOF,         // End of file
+} TokenKind;
+
+// Token type
+typedef struct Token Token;
+struct Token {
+    TokenKind kind; // Token kind
+    Token *next;    // Next token
+    int val;        // If kind is TK_NUM, its value
+    char *loc;      // Location of token
+    int len;        // Length of token
+};
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+void error_tok(Token *tok, char *fmt, ...);
+bool equal(Token *tok, char *op);
+Token *skip(Token *tok, char *op);
+Token *tokenize(char *input);
+
+//
+// parse.c
+//
+
+// Local variable
+typedef struct Obj Obj;
+struct Obj {
+    Obj *next;
+    char *name; // Variable name
+};
+
+// Function
+typedef struct Function Function;
+struct Function {
+    Node *body;
+    Obj *locals;
+    int stack_size;
+};
+
+// AST node
+typedef enum {
+    ND_ADD,       // +
+    ND_SUB,       // -
+    ND_MUL,       // *
+    ND_DIV,       // /
+    ND_NEG,       // -
+    ND_EQ,        // ==
+    ND_NE,        // !=
+    ND_LT,        // <
+    ND_LE,        // <=
+    ND_ASSIGN,    // =
+    ND_RETURN,    // "return"
+    ND_IF,        // "if"
+    ND_BLOCK,     // { ... }
+    ND_EXPR_STMT, // Expression statement
+    ND_VAR,       // Variable
+    ND_NUM,       // Integer
+} NodeKind;
+
+// AST node type
+struct Node {
+    NodeKind kind; // Node kind
+    Node *next;    // Next node
+
+    Node *lhs;     // Left-hand side
+    Node *rhs;     // Right-hand side
+
+    // "if" statement
+    Node *cond;
+    Node *then;
+    Node *els;
+
+    // Block
+    Node *body;
+
+    Obj *var;      // If kind is ND_VAR, its object
+    int val;       // If kind is ND_NUM, its value
+};
+
+Function *parse(Token *tok, Const *cons);
+
+//
+// codegen.c
+//
+
+void codegen(Function *prog, Const *cons);
+
+//
+// helpers.c
+//
+
+void print_tokens(Token *tok);
+void walk_ast(Node *node, int tablevel);
+
+#endif
---
+++ commit-15/test.py
@@ -0,0 +1,64 @@
+import subprocess
+
+
+def assert_ret(expected, input):
+    _ = subprocess.getoutput(f"./chibicc '{input}' > tmp.yas")
+    _ = subprocess.getoutput("yamasm tmp.yas tmp")
+    output = subprocess.getoutput("yamini tmp").strip()
+
+    if output == expected:
+        print(f"{input} => {expected}")
+    else:
+        print(f"\033[91mError: \033[mExpected {expected}, but got {output}")
+
+
+def main():
+    assert_ret("0", "{ return 0; }")
+    assert_ret("42", "{ return 42; }")
+    assert_ret("21", "{ return 5+20-4; }")
+    assert_ret("41", "{ return 12 + 34 - 5 ; }")
+    assert_ret("47", "{ return 5+6*7; }")
+    assert_ret("15", "{ return 5*(9-6); }")
+    assert_ret("4", "{ return (3+5)/2; }")
+    assert_ret("10", "{ return -10+20; }")
+    assert_ret("10", "{ return - -10; }")
+    assert_ret("10", "{ return - - +10; }")
+
+    assert_ret("0", "{ return 0==1; }")
+    assert_ret("1", "{ return 42==42; }")
+    assert_ret("1", "{ return 0!=1; }")
+    assert_ret("0", "{ return 42!=42; }")
+
+    assert_ret("1", "{ return 0<1; }")
+    assert_ret("0", "{ return 1<1; }")
+    assert_ret("0", "{ return 2<1; }")
+    assert_ret("1", "{ return 0<=1; }")
+    assert_ret("1", "{ return 1<=1; }")
+    assert_ret("0", "{ return 2<=1; }")
+
+    assert_ret("1", "{ return 1>0; }")
+    assert_ret("0", "{ return 1>1; }")
+    assert_ret("0", "{ return 1>2; }")
+    assert_ret("1", "{ return 1>=0; }")
+    assert_ret("1", "{ return 1>=1; }")
+    assert_ret("0", "{ return 1>=2; }")
+
+    assert_ret("3", "{ a=3;  return a; }")
+    assert_ret("8", "{ a=3; z=5;  return a+z; }")
+    assert_ret("3", "{ foo=3;  return foo; }")
+    assert_ret("8", "{ foo123=3; bar=5;  return foo123+bar; }")
+
+    assert_ret("1", "{ return 1; 2; 3; }")
+    assert_ret("2", "{ 1; return 2; 3; }")
+    assert_ret("3", "{ 1; 2; return 3; }")
+
+    assert_ret("3", "{ if (0) return 2; return 3; }")
+    assert_ret("3", "{ if (1-1) return 2; return 3; }")
+    assert_ret("2", "{ if (1) return 2; return 3; }")
+    assert_ret("2", "{ if (2-1) return 2; return 3; }")
+    assert_ret("4", "{ if (0) { 1; 2; return 3; } else { return 4; } }")
+    assert_ret("3", "{ if (1) { 1; 2; return 3; } else { return 4; } }")
+
+    
+if __name__ == "__main__":
+    main()
